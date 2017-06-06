9e26ed98878c782979c631c61223bca5546e1373
randomraymon <ramon.rohrbach@stud.hslu.ch>
2017-06-06 16:40:40 +0200

    first commit

---
+++ jquery/main.js
@@ -0,0 +1,28 @@
+// JavaScript Document
+
+
+
+/* show & hide ---------------------------------------------------- */
+
+
+//document.ontouchmove = function(event){
+  //  event.preventDefault();
+//}
+
+$(document).ready(function() {
+
+	$('.arrowstart').click(function(){
+			console.log("hellyeah");
+		
+			$(".test").hide();
+		
+	});
+	
+	$(".sanfranciscobutton").click(function(){
+	  
+	  console.log("itworks");
+	  	
+	  
+	  });
+	
+});
